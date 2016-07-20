1ed6d945237b71e04e6f47a03e4423019d87fd47
golanguz <uzbekish.dictionary@gmail.com>
2016-07-20 16:49:03 +0500

    add qisqus

---
+++ code/salom.go
@@ -0,0 +1,15 @@
+// Salom Gopherbek
+
+package main // paket nomini aniqlaymiz
+
+
+import "fmt" // fmt paketini import qilamiz 
+
+
+func main() { // bosh funksiya aniqlaymiz
+	fmt.Println("Salom, Gopherbek") // fmt paketida .Println funksiyasini
+// qullanamiz va "Salom, Gopherbek" so'zini terminalga yozib chiqaramiz
+)
+
+
+
