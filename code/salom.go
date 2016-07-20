7083e681def8ea03a1c19a3499793124aa668163
golanguz <uzbekish.dictionary@gmail.com>
2016-07-20 20:38:30 +0500

    added linux and windows installation tuts2

--- code/salom.go
+++ code/salom.go
@@ -9,7 +9,7 @@ import "fmt" // fmt paketini import qilamiz
 func main() { // bosh funksiya aniqlaymiz
 	fmt.Println("Salom, Gopherbek") // fmt paketida .Println funksiyasini
 // qullanamiz va "Salom, Gopherbek" so'zini terminalga yozib chiqaramiz
-)
+}
 
 
 
